7c2e743e6783630fab76136e4a2eb591a6d2b9ca
greegor1 <grzegorz27393@gmail.com>
2019-09-28 01:20:23 +0200

    note model, notes-item, notes-list, NotesService

---
+++ src/app/notes/notes-item/notes-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Note } from '../note.model';
+import { NotesService } from '../notes.service';
+
+@Component({
+  selector: 'app-notes-item',
+  templateUrl: './notes-item.component.html',
+  styleUrls: ['./notes-item.component.scss'],
+  providers: [NotesService]
+})
+export class NotesItemComponent implements OnInit {
+  @Input() note: Note;
+
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+  }
+
+  onSelected() {
+    this.notesService.noteSelected.emit(this.note);
+  }
+}
---
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from '../note.model';
+import { NotesService } from '../notes.service';
+
+@Component({
+  selector: 'app-notes-list',
+  templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.scss'],
+  providers: [NotesService]
+})
+export class NotesListComponent implements OnInit {
+  notes: Note[];
+
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+    this.notes = this.notesService.getNote();
+  }
+
+}
