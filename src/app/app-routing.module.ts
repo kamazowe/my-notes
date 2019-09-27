80a2a92a4daa18d019ee6e5cb85b5f4af9dfbd37
greegor1 <grzegorz27393@gmail.com>
2019-09-27 11:36:52 +0200

    bootstrap i, loginFieldComponent, 2 inputs, button dissabled/enabled, root path: 'notes'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFieldComponent } from './login-field/login-field.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'notes', component: LoginFieldComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
